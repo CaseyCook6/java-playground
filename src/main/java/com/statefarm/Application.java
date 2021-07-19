b7855025765f3d704ee6f117052055fa48bab8d1
Casey Cook <casey.cook.eher@statefarm.com>
2021-07-19 16:29:51 -0500

    add working code for email and name formatter

--- src/main/java/com/statefarm/Application.java
+++ src/main/java/com/statefarm/Application.java
@@ -2,7 +2,13 @@ package com.statefarm;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hello world");
-    }
 
+        if (args.length == 0) {
+            System.out.println("Please specify a name and email");
+        } else if (args[1] == null) {
+            System.out.println("Please specify an email for " + args[0]);
+        } else {
+            System.out.println(args[0] + " <" + args[1] + ">");
+        }
+    }
 }
